502ed5430210a4f701fbc430335e30db1feab0ac
Daniel James <daniel.james@alice.it>
2021-05-27 16:48:41 +0200

    Project setup

---
+++ NBA-app/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  NBA-app
+//
+//  Created by Daniel James Tronca on 27/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
